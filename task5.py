34faba78d85805a77f8ada3421f699dfa5c763e3
maria antonietta <mariaantoietta.bruno24@gmail.com>
2023-05-23 11:54:17 +0200

    Added some generic function for task5

--- task5.py
+++ task5.py
@@ -1,5 +1,5 @@
 from tqdm import tqdm
-
+from collections import Counter
 import database as DBFunc
 import task2
 
@@ -19,10 +19,13 @@ def processing(Caltech101,ReteNeurale):
     # calcolare grafo
     listVertici,listArchi = getGraph(DB,n)
     print(listVertici)
-    print(listArchi)
+    print(listArchi[0])
     # calcolare MT matrice transizione
+    M = getMatrix(len(DB))
+    print(M.shape)
     # calcolare matrice di teletrasporto
     # sommare le matrici
+ 
     # trovare autovettore con autovalore 1
     # prendere le m immagini più significative
 
@@ -30,7 +33,6 @@ def getGraph(dataset, n):
     listVertici=[]
     listArchi=[]
 
-    tqdm.tqdm = False
     for i in range(len(dataset)):
         listVertici.append(DBFunc.getIDfromRow(i+1))
 
@@ -43,4 +45,33 @@ def getGraph(dataset, n):
 
     return listVertici, listArchi
 
+#crea la matrice M con liste vuote 
+def getMatrix(N,empty):
+       M = []
+       if empty == True:
+        for _ in range(N):
+            M.append([])
+       else: 
+         for _ in range(N):
+            M.append([1/N])
 
+       return M
+
+#riempie la matrice M 
+def fillMMatrix(M,N, listArchi, counter):
+    for i in range(N): 
+        for j in range(N):
+            if (i,j) in listArchi or (j,i) in listArchi:
+                  M[i][j] = counter[i]
+            else:
+               M[i][j] = 0
+        
+
+#crea un dizionario che ha l'id dell'arco e quante volte questo compare come primo elemento della tupla 
+def getOuterEdges(listArchi):
+     c = Counter(el[0] for el in listArchi)
+
+def getZMatrix(Beta,N,M):
+     tmp = getMatrix(N,False)
+     Z = (1-Beta)*tmp+Beta*M
+     return Z
