5af85919ac0b1a8b6a605516a79ffda9c89f6250
Giorgio Mecca <g.mecca@reply.it>
2023-05-24 15:10:23 +0200

    task 2_1 save Distance in File

---
+++ task2_1.py
@@ -0,0 +1,41 @@
+import csv
+from tqdm import tqdm
+
+import database as DBFunc
+import task2
+def processing(Caltech101, ReteNeurale):
+
+    # ask ID spase
+    ID_Space = DBFunc.IDSpace()
+    # take DB: matrix corresponding to the csv file indicated by the ID_space
+    DB = DBFunc.getDB(ID_Space)
+
+    ID_Distance= takeIDDistance()
+
+    fileDistance= takeFileDistance(ID_Distance,ID_Space)
+    writerDistance = csv.writer(fileDistance, delimiter=';')
+
+
+    for img in tqdm(DB):
+        distanze=task2.getSimilarityVector(img,DB)
+        writerDistance.writerow(distanze)
+
+def takeIDDistance():
+    ID_Distance = ''
+    while ID_Distance != '1':
+        ID_Distance = input("select space: \n 1 - euclidean \nChoice:  ")
+        if (ID_Distance != '1'):
+            print("insert a valid selection")
+    return int(ID_Distance)
+
+def takeFileDistance(ID_Distance,ID_Space):
+    fileDistance = {}
+    if ID_Distance == 1:
+        if ID_Space == 1:
+            fileDistance = open('Distance\EuclideanDistance_Layer3.csv', 'w')
+        elif ID_Space == 2:
+            fileDistance = open('Distance\EuclideanDistance_AVGPool.csv', 'w')
+        elif ID_Space == 3:
+            fileDistance = open('Distance\EuclideanDistance_Last.csv', 'w')
+
+    return fileDistance
