482876cc352b8c46798938f730b91c1be51edf67
Giorgio Mecca <g.mecca@reply.it>
2023-05-31 14:55:32 +0200

    task6 kmeans and save csv file (creare cartella LatentFeatures)

--- task6.py
+++ task6.py
@@ -1,6 +1,9 @@
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.decomposition import LatentDirichletAllocation as LDA
+from sklearn.cluster import KMeans
+import csv
+from tqdm import tqdm
 
 import database as DBFunc
 import genericFunction as GF
@@ -8,16 +11,18 @@ import genericFunction as GF
 def processing(Caltech101,ReteNeurale):
     # prendere DB
     ID_space = DBFunc.IDSpace()
+    id_row= DBFunc.getDBID()
 
     # prendere k (features latenti)
     k = int(input("inserisci k (features latenti): "))
 
-    #prendere tecnica
+    # prendere tecnica
     redDimID = getRedDim()
 
     # take DB
     DB = DBFunc.getDB(ID_space)
 
+    #featuresLatenti = lista di obj, per ogni obj ha una lista con i k valori per le k feature latenti
     featuresLatenti = None
     if redDimID == 1:
         featuresLatenti = get_PCA(DB,k)
@@ -26,10 +31,17 @@ def processing(Caltech101,ReteNeurale):
     elif redDimID == 3:
         featuresLatenti = get_LDA(DB,k)
     elif redDimID == 4:
-        print("ciao")
+        featuresLatenti = get_KMeans(DB,k)
 
-    print(featuresLatenti)
-    saveFeaturesLatenti(featuresLatenti)
+    #re shape featuresLatenti come lista di feature, ogni feature è una lista di coppie (ID obj, valore)
+    featuresLatentiShape = []
+    for i in tqdm(range(k)):
+        d= []
+        for j in range(len(featuresLatenti)):
+            d.append( (DBFunc.getIDfromRow(j+1,id_row) , featuresLatenti[j][i]) )
+        featuresLatentiShape.append(d)
+
+    saveFeaturesLatenti(featuresLatentiShape,ID_space,redDimID)
 
 
 def getRedDim():
@@ -62,12 +74,41 @@ def get_LDA(DB,k):
 
     return res
 
-def saveFeaturesLatenti(featuresLatenti):
-    print("salva")
-
 def getArgMin(DB):
     argmin= DB[0][0]
     for i in range(len(DB)):
         if min(DB[i])<argmin:
             argmin=min(DB[i])
     return argmin
+
+def get_KMeans(DB,k):
+    kmeans = KMeans(n_clusters=k, n_init='auto')
+    kmeans.fit(DB)
+    res = kmeans.transform(DB)
+
+    return res
+
+def saveFeaturesLatenti(featuresLatenti,ID_space,redDimID):
+    name = "featuresK"
+    if ID_space == 1:
+        name+="_Layer3_"
+    elif ID_space == 2:
+        name+="_AVGPool_"
+    elif ID_space == 3:
+        name+="_VectorLast_"
+
+    if redDimID == 1:
+        name+="_PCA"
+    elif redDimID == 2:
+        name+="_SVD"
+    elif redDimID == 3:
+        name += "_LDA"
+    elif redDimID == 4:
+        name += "_KMeans"
+
+    print("salvataggio su File")
+    file = open('LatentFeatures\\' + name + '.csv', 'w', newline='')
+    writer = csv.writer(file, delimiter=';')
+
+    for row in featuresLatenti:
+        writer.writerow(row)
