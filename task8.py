ccc3f3a0d574fe4974ed8c0636f888b7ad8de1fa
Giorgio Mecca <g.mecca@reply.it>
2023-06-05 14:10:33 +0200

    task7 fix DB

--- task8.py
+++ task8.py
@@ -5,89 +5,61 @@ import genericFunction as GF
 import task2
 import task3
 import task6
-def processing(Caltech101,ReteNeurale):
-    # prendere DB
-    ID_space = DBFunc.IDSpace()
-    id_row = DBFunc.getDBID()
-
-    # prendere k (features latenti)
-    k = int(input("inserisci k (features latenti): "))
-
-    # prendere tecnica
-    redDimID = task6.getRedDim()
+import task7
+def processing(dataset,ReteNeurale):
 
     # take DB
+    ID_space = DBFunc.IDSpace()
+    id_row = DBFunc.getDBID()
     DB = DBFunc.getDB(ID_space)
 
-    DB = createDBLabelLabel(Caltech101, DB, id_row)
+    # take k (latent features)
+    k = int(input("inserisci k (features latenti): "))
 
-    # featuresLatenti = lista di obj, per ogni obj ha una lista con i k valori per le k feature latenti
+    # take id of method
+    redDimID = task6.getRedDim()
+
+    # take DB Label-Label
+    DBLabelDistance = createDBLabelLabel(dataset, DB, id_row)
+
+    # featuresLatenti = list of obj, for each obj it has a list with the k values for the k latent features
     featuresLatenti = None
     if redDimID == 1:
-        featuresLatenti = task6.get_PCA(DB, k)
+        featuresLatenti = task6.get_PCA(DBLabelDistance, k)
     elif redDimID == 2:
-        featuresLatenti = task6.get_SVD(DB, k)
+        featuresLatenti = task6.get_SVD(DBLabelDistance, k)
     elif redDimID == 3:
-        featuresLatenti = task6.get_LDA(DB, k)
+        featuresLatenti = task6.get_LDA(DBLabelDistance, k)
     elif redDimID == 4:
-        featuresLatenti = task6.get_KMeans(DB, k)
+        featuresLatenti = task6.get_KMeans(DBLabelDistance, k)
 
-    featuresLatentiShape = []
-    for i in tqdm(range(k)):
-        d = []
-        for j in range(len(featuresLatenti)):
-            d.append((j, featuresLatenti[j][i]))
-        d = sorted(d, key=lambda tup: tup[1], reverse=True)
-        featuresLatentiShape.append(d)
+    # reshape featuresLatenti come as a list of features, each feature is a list of pairs (id Label, value)
+    featuresLatentiShape = task7.shapeLatentFeatures(featuresLatenti, k, id_row)
 
+    # save latent features on file
     GF.saveOnFileLatentFeatures("featuresKTask8",featuresLatentiShape,ID_space,redDimID)
 
-def createDBLabelLabel(Caltech101, DB, id_row):
-    DBLabel = task3.getDatasetLabel(Caltech101, DB, id_row)
-    DBLabel= shapeDBLabel(DBLabel)
-    DBLabel = calculateDistanceDB(DBLabel)
+# createDBLabelLabel(dataset, DB, id_row)
+# dataset: dataset of images
+# DB: matrix Image-Features DB[i][j]= value for feature j in img i
+# id_row: Row-ID matching matrix (row in matrix code - ID in dataset)
+#
+# return/create DB(matrix) Label-Label
+def createDBLabelLabel(dataset, DB, id_row):
+    DictDBLabel = task3.getDictDatasetLabel(dataset, DB, id_row)
+    DBLabel = task3.shapeDBLabel(DictDBLabel)
+    DBLabelDistance = calculateLabelDistanceDB(DBLabel)
 
-    return DBLabel
+    return DBLabelDistance
 
-def shapeDBLabel(dataset):
-    res=[]
-    for i in dataset:
-        vectorLeader = dataset[i][0]
-        for img in dataset[i]:
-            vectorLeader += img
-            vectorLeader = [x/2 for x in vectorLeader]
-        res.append(vectorLeader)
-    return res
-
-def calculateDistanceDB(dataset):
+# calculateLabelDistanceDB(dataset)
+# dataset: matrix Label-Features
+#
+# calculate for each label the distance from all the other labels
+def calculateLabelDistanceDB(dataset):
     res= []
     for label in tqdm(dataset):
-        distanze= task2.getSimilarityVector(label,dataset)
-        distanze= [j.detach().numpy().item() for (i,j) in distanze]
+        distanze = task2.getSimilarityVector(label,dataset)
+        distanze = [j.detach().numpy().item() for (i,j) in distanze]
         res.append(distanze)
     return res
-
-def saveFeaturesLatenti(featuresLatenti,ID_space,redDimID):
-    name = "featuresK"
-    if ID_space == 1:
-        name+="_Layer3_"
-    elif ID_space == 2:
-        name+="_AVGPool_"
-    elif ID_space == 3:
-        name+="_VectorLast_"
-
-    if redDimID == 1:
-        name+="_PCA"
-    elif redDimID == 2:
-        name+="_SVD"
-    elif redDimID == 3:
-        name += "_LDA"
-    elif redDimID == 4:
-        name += "_KMeans"
-
-    print("salvataggio su File")
-    file = open('LatentFeatures\\' + name + '.csv', 'w', newline='')
-    writer = csv.writer(file, delimiter=';')
-
-    for row in featuresLatenti:
-        writer.writerow(row)
